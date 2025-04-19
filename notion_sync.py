8313bbe8aa767207c20efedd93ad422d1423cb72
mimal77777 <mimal77777@gmail.com>
2025-04-19 11:31:53 +0200

    Update notion_sync.py

--- notion_sync.py
+++ notion_sync.py
@@ -1,14 +1,15 @@
 import requests
+import os
 
-NOTION_API_KEY = "YOUR_NOTION_API_KEY"
-DATABASE_ID = "YOUR_DATABASE_ID"
+NOTION_API_KEY = os.environ.get("NOTION_TOKEN")
+DATABASE_ID = os.environ.get("PAGE_ID")
 
 def add_to_notion(message):
     url = "https://api.notion.com/v1/pages"
     headers = {
         "Authorization": f"Bearer {NOTION_API_KEY}",
         "Content-Type": "application/json",
-        "Notion-Version": "2022-06-28",
+        "Notion-Version": "2022-06-28"
     }
     data = {
         "parent": { "database_id": DATABASE_ID },
